0826489cc2eb7679ab31fb470d6f0031f7e21c13
Anurag Shukla <anuragsh@usc.edu>
2016-05-12 17:10:54 -0700

    added files commit

---
+++ deployments/deployment_4/ErmrestHandler.py
@@ -0,0 +1,58 @@
+__author__ = 'anurag'
+import requests
+import json
+
+
+class ErmrestHandler(object):
+    def __init__(self, host, username, password):
+        self.host = host
+        self._cookie = {"ermrest": self.get_cookie(username, password)}
+
+    def get_cookie(self, username, password):
+        r = requests.post('http://'+self.host+'/ermrest/authn/session',
+                          data={'username': username,
+                                'password': password})
+        r.raise_for_status()
+        return r.text[:-1]
+
+    def create_catalog(self):
+        pass
+
+    def create_schema(self, name):
+        pass
+
+    def create_table(self, name):
+        pass
+
+    def get_data(self, catalog_id, table_name, user_name):
+        r = requests.get("http://"+self.host+"/ermrest/catalog/"+str(catalog_id)+"/entity/"+
+                         table_name+"/user="+user_name, cookies=self._cookie)
+        return json.loads(r.text)
+
+    def put_data(self, catalog_id, table_name, data):
+        data = [data]
+        r = requests.put("http://"+self.host+"/ermrest/catalog/"+str(catalog_id)+"/entity/"+table_name,
+                         json=data, cookies=self._cookie)
+
+        r.raise_for_status()
+
+
+if __name__ == '__main__':
+    conn = ErmrestHandler('ec2-54-172-182-170.compute-1.amazonaws.com', 'root', 'root')
+    conn.put_data(1, 'sample7', {'user': 'Sam'})
+    data = conn.get_data(1, 'sample7', 'Sam')[0]
+    print data
+    data['start_date'] = '2016'
+    conn.put_data(1, 'sample7', data)
+    data = conn.get_data(1, 'sample7', 'Sam')[0]
+    print data
+    data['end_date'] = '2017'
+    conn.put_data(1, 'sample7', data)
+    data = conn.get_data(1, 'sample7', 'Sam')[0]
+    print data
+    data['gel_type'] = 'agarose'
+    conn.put_data(1, 'sample7', data)
+    data = conn.get_data(1, 'sample7', 'Sam')[0]
+    print data
+
+
