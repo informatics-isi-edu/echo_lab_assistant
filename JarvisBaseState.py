3e9db66b3379797f0538034c50ea7f08f7e75681
Aleksandr Stevens <arstevens310@gmail.com>
2016-08-02 13:31:01 -0700

    made more readable

--- JarvisBaseState.py
+++ JarvisBaseState.py
@@ -23,10 +23,11 @@ class JarvisBaseState(object):
 ##=============================Helper Methods=============================================
 
 	def _get_intent(self, intent_request):
+		intent = None 
 		if 'intent' in intent_request:
-			return intent_request['intent']
-        	else:
-            		return None
+			intent =  intent_request['intent']
+		
+		return intent 
 
 	def _get_intent_name(self, intent_request):
 		intent = self._get_intent(intent_request)
@@ -37,11 +38,12 @@ class JarvisBaseState(object):
 		return intent_name
 
 	def _slot_exists(self, slot_name, intent_request):
+		exists = False
 		intent = self._get_intent(intent_request)
 		if intent:
-			return slot_name in intent['slots']
-		else:
-			return False
+			exists = slot_name in intent['slots']
+		
+		return exists
 
 	def _get_slot_value(self, slot_name, intent_request):
 		value = None
@@ -64,33 +66,36 @@ class JarvisBaseState(object):
 		return current_user
 	
 	def _set_session_data(self,column,new_data):
+		success = False
 		current_data = self._ermrest.get_data(7,"session_info")[0]
 		current_data[column] = new_data
 		try:
 			self._ermrest.delete_data(7,"session_info")
 			self._ermrest.put_data(7,"session_info",current_data)
-			return True
+			success = True
 		except Exception as exc:
-			print(str(exc))
-			return False
+			print("[!] ERROR: "+str(exc))
+		
+		return success
 
 	def _set_completed_step(self,new_step):
+		success = False
+		data = {"completed_step":new_step}
 		try:
-			data = self._ermrest.get_data(7,"step_completed")[0]
 			self._ermrest.delete_data(7,"step_completed")
 		except:
-			data = {"completed_step":None}
-
-		data['completed_step'] = new_step
+			pass
 		try:
 			self._ermrest.put_data(7,"step_completed",data)
-			return True
+			success = True
 		except Exception as exc:
 			print('[!] ERROR: '+str(exc))
-			return False
+		
+		return success
 
 	def _clear(self,table_name):
 		#clears a table
+		success = False
 		clean_data = self._ermrest.get_data(7,table_name)[0]
 		for key in clean_data:
 			clean_data[key] = None
@@ -98,10 +103,12 @@ class JarvisBaseState(object):
 		try:
 			self._ermrest.delete_data(7,table_name)
 			self._ermrest.put_data(7,table_name,clean_data)
-			return True
+			success = True
 		except Exception as exc:
 			print(str(exc))
-			return False
+			success = False
+
+		return success
 
 	def _get_last_step(self,experiment_id):
 		experiment = self._ermrest.get_data(7,"experiment_data","/experiment_id="+str(experiment_id))[0]
@@ -111,13 +118,14 @@ class JarvisBaseState(object):
 
 
 	def _get_experiment(self,experiment_id):
+		experiment = dict() 
 		experiment_id = str(experiment_id)
 		user = str(self._get_current_user())
 		query = "/user="+user+"/experiment_id="+experiment_id
 		try:
 			experiment = self._ermrest.get_data(7,"experiment_data",query)
-			return experiment	
 		except Exception as exc:
-			print(str(exc))
-			return {} 
+			print("[!] ERROR: "+str(exc))
+		
+		return experiment
 	
